f83db844d46caf0336ff1e4e6204af1b4337068a
MeliOrdas <294592530@qq.com>
2020-03-18 21:00:05 +0800

    3/18

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <a href="#"></a>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
+@import "./assets/css/base.css";
+
+
 </style>
